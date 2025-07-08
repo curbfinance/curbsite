3fce44ad0f05bd540ffd09f976293b957bcca2a5
curbfinance <curbfinance.fi@gmail.com>
2025-07-08 11:16:21 -0700

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const CURB_TOKEN_MINT = "6HtdRdg6r29sLXd3b5fioeP3TDLy6jQ4LY2dAxFKbonk";
+export const RAYDIUM_LP_ACCOUNT = "5FJSVJkaU1EXGn8xzEnAADJMxapn6bFAT3SogvfgUYi7";
+export const DECIMALS = 9;
+export const SOLANA_RPC = "https://api.mainnet-beta.solana.com";
