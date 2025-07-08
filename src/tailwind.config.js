3787eb393911142d07cd9b14de483b7ffa7eb857
curbfinance <curbfinance.fi@gmail.com>
2025-07-08 11:17:51 -0700

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'curb-purple': '#7C3AED',
+        'curb-blue': '#2563EB',
+      },
+    },
+  },
+  plugins: [],
+}
